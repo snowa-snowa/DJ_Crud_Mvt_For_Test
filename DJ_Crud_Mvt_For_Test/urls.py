53d68dfb20f502132609ea81af4e9443022719d0
snowa-snowa <sione8172635@gmail.com>
2025-07-17 10:14:33 +0500

    ++style

--- DJ_Crud_Mvt_For_Test/urls.py
+++ DJ_Crud_Mvt_For_Test/urls.py
@@ -5,16 +5,10 @@ from django.utils import timezone
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    # Главная страница проекта
     path('', TemplateView.as_view(
         template_name='index.html',
         extra_context={'now': timezone.now()}
     ), name='index'),
-
-    # Ведём на index CRUD‑приложения
     path('crud/', include('crud_app.urls')),
-
-    # Ведём на index MVT‑приложения
     path('mvt/', include('mvt_app.urls')),
 ]
